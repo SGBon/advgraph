1d58ef82e51588f38cc1d60aecc8470fb4db2100
Santiago Bonada <Santiago.Bonada@uoit.net>
2016-10-11 09:42:41 -0400

    updated gitignore, assignment 1 files

---
+++ ass1/tergen.cpp
@@ -0,0 +1,14 @@
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <cmath>
+#include <cstdio>
+#include "Shaders.h"
+
+int main(){
+  printf("hello\n");
+  return 0;
+}
