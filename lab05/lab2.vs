03d04a6d20fc0832dd4793cb83345a2fbc57e8a4
Santiago Bonada <santiago.bonada@uoit.net>
2016-10-17 12:38:33 -0400

    stuff

---
+++ lab05/lab2.vs
@@ -0,0 +1,22 @@
+/*
+ *  Simple vertex shader for lab 4
+ */
+
+#version 330 core
+
+uniform mat4 modelView;
+uniform mat4 projection;
+in vec4 vPosition;
+in vec3 vNormal;
+in vec2 vTex;
+out vec4 position;
+out vec3 normal;
+out vec2 tc;
+
+void main() {
+
+	gl_Position = projection * modelView * vPosition;
+  position = vPosition.xyz;
+	normal = vNormal;
+  tc = vTex;
+}
---
+++ lab05/viewer.cpp
@@ -0,0 +1,263 @@
+/************************************************************
+*                   CSCI 4110 Lab 2
+*
+*  Basic OpenGL program that shows how to set up a
+*  VAO and some basic shaders.  This program draws
+*  a cube or sphere depending upon whether CUBE or
+*  SPHERE is defined.
+*
+*/
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include "Shaders.h"
+#include "tiny_obj_loader.h"
+#include "texture.h"
+#include <iostream>
+
+float eyex, eyey, eyez;
+
+double theta, phi;
+double r;
+
+GLuint program;
+
+glm::mat4 projection;
+
+GLuint objVAO;
+int triangles;
+
+void init() {
+    GLuint vbuffer;
+    GLuint ibuffer;
+    GLuint tbuffer;
+    GLint vPosition;
+    GLint vNormal;
+    GLint vTex;
+    GLfloat *vertices;
+    GLfloat *normals;
+    GLuint *indices;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    int nv;
+    int nn;
+    int ni;
+    int i;
+
+    glGenVertexArrays(1, &objVAO);
+    glBindVertexArray(objVAO);
+
+    /*  Load the obj file */
+
+    std::string err = tinyobj::LoadObj(shapes, materials, "sphere.obj", 0);
+
+    if (!err.empty()) {
+        std::cerr << err << std::endl;
+        return;
+    }
+
+    /*  Retrieve the vertex coordinate data */
+
+    nv = shapes[0].mesh.positions.size();
+    vertices = new GLfloat[nv];
+    for (i = 0; i<nv; i++) {
+        vertices[i] = shapes[0].mesh.positions[i];
+    }
+
+    /*  Retrieve the vertex normals */
+
+    nn = shapes[0].mesh.normals.size();
+    normals = new GLfloat[nn];
+    for (i = 0; i<nn; i++) {
+        normals[i] = shapes[0].mesh.normals[i];
+    }
+
+    /*  Retrieve the triangle indices */
+
+    ni = shapes[0].mesh.indices.size();
+    triangles = ni / 3;
+    indices = new GLuint[ni];
+    for (i = 0; i<ni; i++) {
+        indices[i] = shapes[0].mesh.indices[i];
+    }
+
+    unsigned int verts = nv/3;
+    unsigned int nt = 2*verts;
+    GLfloat *tex = new GLfloat[nt];
+    for(i = 0;i<verts;i++){
+      int x = vertices[3*i];
+      int y = vertices[3*i+1];
+      int z = vertices[3*i+2];
+
+      theta = atan2(x,z);
+      phi = atan2(y,sqrt(x*x+z*z));
+
+      tex[2*i] = (theta+M_PI)/(2*M_PI);
+      tex[2*i+1] = phi / M_PI;
+    }
+
+    /*
+    *  load the vertex coordinate data
+    */
+    glGenBuffers(1, &vbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
+    glBufferData(GL_ARRAY_BUFFER, (nv + nn + nt)*sizeof(GLfloat), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, nv*sizeof(GLfloat), vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, nv*sizeof(GLfloat), nn*sizeof(GLfloat), normals);
+    glBufferSubData(GL_ARRAY_BUFFER,(nv+nn)*sizeof(GLfloat),nt*sizeof(GLfloat),tex);
+
+    /*
+    *  load the vertex indexes
+    */
+    glGenBuffers(1, &ibuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ni*sizeof(GLuint), indices, GL_STATIC_DRAW);
+
+    /* load texture and put into buffer */
+    Texture *texture = loadTexture("checkerboard.jpg");
+    glGenTextures(1,&tbuffer);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D,tbuffer);
+    glTexStorage2D(GL_TEXTURE_2D,1,GL_RGBA8,texture->width,texture->height);
+    glTexSubImage2D(GL_TEXTURE_2D,0,0,0,texture->width,texture->height,GL_RGB,GL_UNSIGNED_BYTE,texture->data);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+
+    /*
+    *  link the vertex coordinates to the vPosition
+    *  variable in the vertex program.  Do the same
+    *  for the normal vectors.
+    */
+    glUseProgram(program);
+    vPosition = glGetAttribLocation(program, "vPosition");
+    glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vPosition);
+
+    vNormal = glGetAttribLocation(program, "vNormal");
+    glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*)(nv*sizeof(GLfloat)));
+    glEnableVertexAttribArray(vNormal);
+
+    vTex = glGetAttribLocation(program,"vTex");
+    glVertexAttribPointer(vTex,2,GL_FLOAT,GL_FALSE,0,(void*)((nv+nn)*sizeof(GLfloat)));
+    glEnableVertexAttribArray(vTex);
+}
+
+void changeSize(int w, int h) {
+
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window of zero width).
+
+    if (h == 0)
+        h = 1;
+
+    float ratio = 1.0 * w / h;
+
+    glViewport(0, 0, w, h);
+
+    projection = glm::perspective(45.0f, ratio, 1.0f, 100.0f);
+
+}
+
+void displayFunc(void) {
+    glm::mat4 view;
+    int viewLoc;
+    int projLoc;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(program);
+
+    view = glm::lookAt(glm::vec3(eyex, eyey, eyez),
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f));
+
+    viewLoc = glGetUniformLocation(program, "modelView");
+    glUniformMatrix4fv(viewLoc, 1, 0, glm::value_ptr(view));
+    projLoc = glGetUniformLocation(program, "projection");
+    glUniformMatrix4fv(projLoc, 1, 0, glm::value_ptr(projection));
+
+    glBindVertexArray(objVAO);
+    glDrawElements(GL_TRIANGLES, 3*triangles, GL_UNSIGNED_INT, NULL);
+
+    glutSwapBuffers();
+}
+
+void keyboardFunc(unsigned char key, int x, int y) {
+
+    switch (key) {
+    case 'a':
+        phi -= 0.1;
+        break;
+    case 'd':
+        phi += 0.1;
+        break;
+    case 'w':
+        theta += 0.1;
+        break;
+    case 's':
+        theta -= 0.1;
+        break;
+		case 'q':
+			r -= 0.1;
+			break;
+		case 'e':
+			r += 0.1;
+			break;
+    }
+
+    eyex = r*sin(theta)*cos(phi);
+    eyey = r*sin(theta)*sin(phi);
+    eyez = r*cos(theta);
+
+    glutPostRedisplay();
+
+}
+
+int main(int argc, char **argv) {
+    int fs;
+    int vs;
+
+		glutInit(&argc, argv);
+		glutInitContextVersion(4,3);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(320, 320);
+    glutCreateWindow("Viewer");
+
+		glewExperimental = GL_TRUE;
+    GLenum error = glewInit();
+    if (error != GLEW_OK) {
+        printf("Error starting GLEW: %s\n", glewGetErrorString(error));
+        exit(0);
+    }
+
+    glutDisplayFunc(displayFunc);
+    glutReshapeFunc(changeSize);
+    glutKeyboardFunc(keyboardFunc);
+
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+
+    vs = buildShader(GL_VERTEX_SHADER, "lab2.vs");
+    fs = buildShader(GL_FRAGMENT_SHADER, "lab2.fs");
+    program = buildProgram(vs, fs, 0);
+    dumpProgram(program, "Lab 2 shader program");
+    init();
+
+    eyex = 0.0;
+    eyez = 0.0;
+    eyey = 10.0;
+
+    theta = 1.5;
+    phi = 1.5;
+    r = 10.0;
+
+    glutMainLoop();
+
+}
